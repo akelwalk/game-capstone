8b2dd15ff698c7549566903832a55370603d7438
akelwalk <104277293+akelwalk@users.noreply.github.com>
2025-02-14 13:46:25 -0700

    cafe progress

---
+++ Assets/Aditi/Scripts/coffee shop/NotebookBackButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.ReorderableList;
+using UnityEngine;
+
+public class NotebookBackButton : MonoBehaviour
+{
+    public GameObject previousMenu;
+    public GameObject nextMenu;
+
+    public void OnClick() {
+        StartCoroutine(SwitchMenus());
+    }
+
+    private IEnumerator SwitchMenus() {
+        yield return new WaitForEndOfFrame(); 
+        nextMenu.SetActive(true);
+        // button.SetActive(true);
+        previousMenu.SetActive(false);
+    }
+
+}

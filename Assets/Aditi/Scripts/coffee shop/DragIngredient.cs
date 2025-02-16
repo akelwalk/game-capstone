a753796f79a97ac37d13ab1c8bc4fd32934ac58f
akelwalk <104277293+akelwalk@users.noreply.github.com>
2025-02-16 15:21:45 -0700

    ingredients

--- Assets/Aditi/Scripts/coffee shop/DragIngredient.cs
+++ Assets/Aditi/Scripts/coffee shop/DragIngredient.cs
@@ -1,18 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DragIngredient : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Drag : MonoBehaviour {
+  private bool dragging = false;
+  private Vector3 offset;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  void Update() {
+    if (dragging) {
+      // Move object, taking into account original offset.
+      transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
     }
+  }
+
+  private void OnMouseDown() {
+    // Record the difference between the objects center, and the clicked point on the camera plane.
+    offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    dragging = true;
+  }
+
+  private void OnMouseUp() {
+    // Stop dragging.
+    dragging = false;
+  }
 }
